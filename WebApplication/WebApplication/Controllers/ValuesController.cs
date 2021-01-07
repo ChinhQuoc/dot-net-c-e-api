95942fd560ca69d56bdc57b9e21abfd80bed43e0
dthiep3012 <49056826+taetae301296@users.noreply.github.com>
2021-01-07 22:20:24 +0700

    first web api

---
+++ WebApplication/WebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WebApplication.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        static List<string> listNames = new List<string>
+        {
+            "Anna", "Hanna", "Yuna"
+        };
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return listNames;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return listNames[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            listNames.Add(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+            listNames[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            listNames.RemoveAt(id);
+        }
+    }
+}
