42dfe5c90effe13e7381380b689e5657ca98ea97
dthiep3012 <49056826+taetae301296@users.noreply.github.com>
2021-01-10 00:52:17 +0700

    connect sql server

---
+++ WebApplication/WebApplication/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApplication.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using(EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                return dbContext.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using(EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                return dbContext.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
